8f3f4d3b9a9986e6905bb9b17a626accc5e574b2
bhaveshppatil <bhavesh.patil0325@gmail.com>
2021-07-22 19:17:13 +0530

    Updated From Bhavesh

--- app/src/main/java/com/example/happnclone/DataPolicy.java
+++ app/src/main/java/com/example/happnclone/DataPolicy.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.happnclone.Quiz.MaleFemaleCheck;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -43,8 +42,6 @@ public class DataPolicy extends AppCompatActivity {
                 signIn();
             }
         });
-
-        
     }
 
     public void signIn() {
@@ -55,19 +52,12 @@ public class DataPolicy extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
-            if (task.isSuccessful()){
-                Intent intent = new Intent(DataPolicy.this, MaleFemaleCheck.class);
-                startActivity(intent);
-            }
-
-
         }
     }
 
@@ -83,9 +73,10 @@ public class DataPolicy extends AppCompatActivity {
                 String personId = acct.getId();
                 Uri personPhoto = acct.getPhotoUrl();
 
+                Intent intent = new Intent();
+                intent.putExtra("personPhoto", acct.getPhotoUrl());
                 Toast.makeText(DataPolicy.this, "Welcome " + personName + "to Happn App lets find yor " +
                         "girl .... ! UUREEEE ", Toast.LENGTH_LONG).show();
-
             }
 
         } catch (ApiException e) {
--- app/src/main/java/com/example/happnclone/Quiz/QuizIntro.java
+++ app/src/main/java/com/example/happnclone/Quiz/QuizIntro.java
@@ -6,18 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.happnclone.R;
 
 public class QuizIntro extends AppCompatActivity {
 
     Button mBtnLetsGO;
+    ImageView userProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_intro);
         mBtnLetsGO = findViewById(R.id.BtnLetsGO);
+        userProfile = findViewById(R.id.IvuserImage);
         mBtnLetsGO.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
